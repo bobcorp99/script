0b6b44bf60faace3b8b949147714217316f8c3e0
bobcorp99 <bobcorp99@mail.ru>
2020-09-17 22:21:17 +0500

    ver_1.0

---
+++ docs/script.js
@@ -0,0 +1,10 @@
+// console.log('salom miwqila')
+var age = prompt('Yoqtirgan avtomobilingiz')
+
+if(age == 'volga' || age == 'Volga' || age == 'волга' || age == 'Волга')
+{
+    alert("Fkusga gap yo brat!")
+}
+else {
+    alert("Fkusis yog'akanu brat")
+}
