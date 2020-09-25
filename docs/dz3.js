a4525ae6a0bc4d8cbeaa493cd867707e4b2b10ef
bobcorp99 <bobcorp99@mail.ru>
2020-09-25 18:26:35 +0500

    ver 2.3

--- docs/dz3.js
+++ docs/dz3.js
@@ -2,10 +2,10 @@ var a = +prompt('1 - sonni kiriting ')
 var b = +prompt('2 - sonni kiriting ')
 var c = +prompt('3 - sonni kiriting ')
 
-if( b < a < c && c < a < b) {
+if((a > c && a < b) || (a > b && a < c)) {
     alert("o'rta qiymat " + a);
-} else if(b < c < a && a < c < b) {
+} else if((c > a && c < b) || (c > b && c < a)) {
     alert("o'rta qiymat " + c) 
-} else if(a < b < c && c < b < a) {
+} else if((b > c && b < a) || (b > a && b < c)) {
     alert("o'rta qiymat " + b)
 }
